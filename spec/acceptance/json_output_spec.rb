607c912db56a97a01d8cd2be15b3da525e08894f
Eric Meyer <emeyer@8thlight.com>
2019-05-05 15:34:54 -0500

    Update Parser to require reporter

--- spec/acceptance/json_output_spec.rb
+++ spec/acceptance/json_output_spec.rb
@@ -9,11 +9,7 @@ describe "Outputting JSON" do
   def parse_input(filename)
     output = StringIO.new
     reporter = Dotpretty::Reporters::Json.new({ output: output })
-    parser = Dotpretty::Parser.new({
-      output: output,
-      reporter: reporter
-    })
-
+    parser = Dotpretty::Parser.new({ reporter: reporter })
     Fixtures.each_line(filename) do |line|
       parser.parse_line(line)
     end
